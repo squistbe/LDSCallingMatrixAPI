9472db43449501310dc407608dae2ed5d1433a15
Spencer Quistberg <quistber@adobe.com>
2017-02-28 15:14:29 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var express  = require('express');
+var app      = express();
+var mongoose = require('mongoose');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+var router = require('./app/routes');
+
+mongoose.connect(databaseConfig.url);
+
+var port = process.env.PORT || 8080;
+app.listen(port);
+console.log("App listening on port " + port);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(logger('dev'));
+app.use(cors());
+
+router(app);
